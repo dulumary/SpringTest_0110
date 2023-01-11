e1790fd18bdba1a1acfa4e345cac8306e513a2cd
asdf <asdf@gmail.com>
2023-01-11 21:37:59 +0900

    [김인규]  - jsp 연동 문제 풀이

--- src/main/java/com/marondal/spring/test/lifecycle/Test02RestController.java
+++ src/main/java/com/marondal/spring/test/lifecycle/Test02RestController.java
@@ -5,13 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marondal.spring.test.lifecycle.model.Post;
+
 @RestController
+@RequestMapping("/lifecycle/test02")
 public class Test02RestController {
 	
-	@RequestMapping("/lifecycle/test02/1")
+	@RequestMapping("/1")
 	public List<Map<String, Object>> listMapResponse() {
 		
 		List<Map<String, Object>> movieList = new ArrayList<>();
@@ -40,5 +45,54 @@ public class Test02RestController {
 		return movieList;
 		
 	}
+	
+	@RequestMapping("/2")
+	public List<Post> listObjectResponse() {
+		
+		List<Post> postList = new ArrayList<>();
+		Post post = new Post();
+		post.setTitle("안녕하세요 가입인사 드립니다.");
+		post.setUser("hagulu");
+		post.setContent("안녕하세요 가입했어요. 앞으로 잘 부탁 드립니다!!");
+		
+		postList.add(post);
+		
+		post = new Post();
+		post.setTitle("헐 대박");
+		post.setUser("bada");
+		post.setContent("오늘 수요일이었어, 금요일인줄");
+		
+		postList.add(post);
+		
+		post = new Post();
+		post.setTitle("오늘 데이트 한 이야기 해드릴게요");
+		post.setUser("dulumary");
+		post.setContent("....");
+		
+		postList.add(post);
+		
+		return postList;
+		
+	}
+	
+	
+	@RequestMapping("/3")
+	public ResponseEntity<Post> entity() {
+		Post post = new Post();
+		post.setTitle("안녕하세요 가입인사 드립니다.");
+		post.setUser("hagulu");
+		post.setContent("안녕하세요 가입했어요. 앞으로 잘 부탁 드립니다!!");
+		
+		ResponseEntity<Post> entity = new ResponseEntity(post, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return entity;
+	}
+	
 
+	
+	
+	
+	
+	
+	
 }

85513d2a480ad0ae9b970c6a01ab8422b6ee728a
asdf <asdf@gmail.com>
2023-01-12 21:28:00 +0900

    [김인규]  - database 설정 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/database/bo/StoreBO.java
@@ -0,0 +1,25 @@
+package com.marondal.spring.test.database.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.database.dao.StoreDAO;
+import com.marondal.spring.test.database.model.Store;
+
+@Service
+public class StoreBO {
+	
+	@Autowired
+	private StoreDAO storeDAO;
+	
+	// store 테이블의 데이터를 조회하고 리턴하는 메소드 
+	public List<Store> getStoreList() {
+		List<Store> storeList = storeDAO.selectStoreList();
+		
+		return storeList;
+		
+	}
+
+}
